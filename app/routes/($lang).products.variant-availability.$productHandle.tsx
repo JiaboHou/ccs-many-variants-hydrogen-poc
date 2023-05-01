24e93ae7ac8b958708d61d9ce87f710fd55ae8a3
Jiabo Hou <jiabo.hou@shopify.com>
2023-05-01 18:53:44 -0400

    refactor: use components from original pdp where possible (#3)

--- app/routes/($lang).products.variant-availability.$productHandle.tsx
+++ app/routes/($lang).products.variant-availability.$productHandle.tsx
@@ -1,34 +1,21 @@
 import {type ReactNode, useRef, Suspense, useMemo, Fragment} from 'react';
-import {Disclosure, Listbox} from '@headlessui/react';
 import {defer, SerializeFrom, type LoaderArgs} from '@shopify/remix-oxygen';
 import {
   useLoaderData,
   Await,
-  useSearchParams,
   useLocation,
-  useNavigation,
   useAsyncValue,
 } from '@remix-run/react';
 
-import {
-  AnalyticsPageType,
-  Money,
-  ShopifyAnalyticsProduct,
-  ShopPayButton,
-} from '@shopify/hydrogen';
+import {AnalyticsPageType, ShopifyAnalyticsProduct} from '@shopify/hydrogen';
 import {
   Heading,
-  IconCaret,
-  IconCheck,
-  IconClose,
   ProductGallery,
   ProductSwimlane,
   Section,
   Skeleton,
   Text,
   Link,
-  AddToCartButton,
-  Button,
 } from '~/components';
 import {getExcerpt} from '~/lib/utils';
 import {seoPayload} from '~/lib/seo.server';
@@ -37,20 +24,20 @@ import clsx from 'clsx';
 import type {
   ProductVariant,
   SelectedOptionInput,
-  PageInfo,
   Product as ProductType,
   Shop,
-  ProductConnection,
-  ProductVariantConnection,
-  Scalars,
   ProductOption,
   SelectedOption,
 } from '@shopify/hydrogen/storefront-api-types';
-import {MEDIA_FRAGMENT, PRODUCT_CARD_FRAGMENT} from '~/data/fragments';
+import {MEDIA_FRAGMENT} from '~/data/fragments';
 import type {Storefront} from '~/lib/type';
 import type {Product} from 'schema-dts';
 import {routeHeaders, CACHE_SHORT} from '~/data/cache';
-import {Maybe} from 'graphql/jsutils/Maybe';
+import {
+  getRecommendedProducts,
+  ProductDetail,
+  ProductForm,
+} from '~/routes/($lang).products.$productHandle';
 
 export const headers = routeHeaders;
 
@@ -151,7 +138,7 @@ export default function Product() {
                   <Text className={'opacity-50 font-medium'}>{vendor}</Text>
                 )}
               </div>
-              <ProductForm />
+              <ProductForm ProductOptionsComponent={ProductOptions} />
               <div className="grid gap-4 py-4">
                 {descriptionHtml && (
                   <ProductDetail
@@ -192,124 +179,6 @@ export default function Product() {
   );
 }
 
-export function ProductForm() {
-  const {product, analytics, storeDomain} = useLoaderData<typeof loader>();
-
-  const [currentSearchParams] = useSearchParams();
-  const {location} = useNavigation();
-
-  /**
-   * We update `searchParams` with in-flight request data from `location` (if available)
-   * to create an optimistic UI, e.g. check the product option before the
-   * request has completed.
-   */
-  const searchParams = useMemo(() => {
-    return location
-      ? new URLSearchParams(location.search)
-      : currentSearchParams;
-  }, [currentSearchParams, location]);
-
-  const firstVariant = product.variants.nodes[0];
-
-  /**
-   * We're making an explicit choice here to display the product options
-   * UI with a default variant, rather than wait for the user to select
-   * options first. Developers are welcome to opt-out of this behavior.
-   * By default, the first variant's options are used.
-   */
-  const searchParamsWithDefaults = useMemo<URLSearchParams>(() => {
-    const clonedParams = new URLSearchParams(searchParams);
-
-    for (const {name, value} of firstVariant.selectedOptions) {
-      if (!searchParams.has(name)) {
-        clonedParams.set(name, value);
-      }
-    }
-
-    return clonedParams;
-  }, [searchParams, firstVariant.selectedOptions]);
-
-  /**
-   * Likewise, we're defaulting to the first variant for purposes
-   * of add to cart if there is none returned from the loader.
-   * A developer can opt out of this, too.
-   */
-  const selectedVariant = product.selectedVariant ?? firstVariant;
-  const isOutOfStock = !selectedVariant?.availableForSale;
-
-  const isOnSale =
-    selectedVariant?.price?.amount &&
-    selectedVariant?.compareAtPrice?.amount &&
-    selectedVariant?.price?.amount < selectedVariant?.compareAtPrice?.amount;
-
-  const productAnalytics: ShopifyAnalyticsProduct = {
-    ...analytics.products[0],
-    quantity: 1,
-  };
-
-  return (
-    <div className="grid gap-10">
-      <div className="grid gap-4">
-        <ProductOptions
-          options={product.options}
-          searchParamsWithDefaults={searchParamsWithDefaults}
-        />
-        {selectedVariant && (
-          <div className="grid items-stretch gap-4">
-            {isOutOfStock ? (
-              <Button variant="secondary" disabled>
-                <Text>Sold out</Text>
-              </Button>
-            ) : (
-              <AddToCartButton
-                lines={[
-                  {
-                    merchandiseId: selectedVariant.id,
-                    quantity: 1,
-                  },
-                ]}
-                variant="primary"
-                data-test="add-to-cart"
-                analytics={{
-                  products: [productAnalytics],
-                  totalValue: parseFloat(productAnalytics.price),
-                }}
-              >
-                <Text
-                  as="span"
-                  className="flex items-center justify-center gap-2"
-                >
-                  <span>Add to Bag</span> <span>·</span>{' '}
-                  <Money
-                    withoutTrailingZeros
-                    data={selectedVariant?.price!}
-                    as="span"
-                  />
-                  {isOnSale && (
-                    <Money
-                      withoutTrailingZeros
-                      data={selectedVariant?.compareAtPrice!}
-                      as="span"
-                      className="opacity-50 strike"
-                    />
-                  )}
-                </Text>
-              </AddToCartButton>
-            )}
-            {!isOutOfStock && (
-              <ShopPayButton
-                width="100%"
-                variantIds={[selectedVariant?.id!]}
-                storeDomain={storeDomain}
-              />
-            )}
-          </div>
-        )}
-      </div>
-    </div>
-  );
-}
-
 function ProductOptions({
   options,
   searchParamsWithDefaults,
@@ -336,71 +205,7 @@ function ProductOptions({
                * First, we render a bunch of <Link> elements for each option value.
                * When the user clicks one of these buttons, it will hit the loader
                * to get the new data.
-               *
-               * If there are more than 7 values, we render a dropdown.
-               * Otherwise, we just render plain links.
                */}
-              {/* {option.values.length > 7 ? (
-                <div className="relative w-full">
-                  <Listbox>
-                    {({open}) => (
-                      <>
-                        <Listbox.Button
-                          ref={closeRef}
-                          className={clsx(
-                            'flex items-center justify-between w-full py-3 px-4 border border-primary',
-                            open
-                              ? 'rounded-b md:rounded-t md:rounded-b-none'
-                              : 'rounded',
-                          )}
-                        >
-                          <span>
-                            {searchParamsWithDefaults.get(option.name)}
-                          </span>
-                          <IconCaret direction={open ? 'up' : 'down'} />
-                        </Listbox.Button>
-                        <Listbox.Options
-                          className={clsx(
-                            'border-primary bg-contrast absolute bottom-12 z-30 grid h-48 w-full overflow-y-scroll rounded-t border px-2 py-2 transition-[max-height] duration-150 sm:bottom-auto md:rounded-b md:rounded-t-none md:border-t-0 md:border-b',
-                            open ? 'max-h-48' : 'max-h-0',
-                          )}
-                        >
-                          {option.values.map((value) => (
-                            <Listbox.Option
-                              key={`option-${option.name}-${value}`}
-                              value={value}
-                            >
-                              {({active}) => (
-                                <ProductOptionLink
-                                  optionName={option.name}
-                                  optionValue={value}
-                                  className={clsx(
-                                    'text-primary w-full p-2 transition rounded flex justify-start items-center text-left cursor-pointer',
-                                    active && 'bg-primary/10',
-                                  )}
-                                  searchParams={searchParamsWithDefaults}
-                                  onClick={() => {
-                                    if (!closeRef?.current) return;
-                                    closeRef.current.click();
-                                  }}
-                                >
-                                  {value}
-                                  {searchParamsWithDefaults.get(option.name) ===
-                                    value && (
-                                    <span className="ml-2">
-                                      <IconCheck />
-                                    </span>
-                                  )}
-                                </ProductOptionLink>
-                              )}
-                            </Listbox.Option>
-                          ))}
-                        </Listbox.Options>
-                      </>
-                    )}
-                  </Listbox>
-                </div>
-              ) : ( */}
               <Suspense>
                 <Await resolve={variants}>
                   <ProductOptionLinks
@@ -409,7 +214,6 @@ function ProductOptions({
                   />
                 </Await>
               </Suspense>
-              {/* )} */}
             </div>
           </div>
         ))}
@@ -555,55 +359,6 @@ function ProductOptionLink({
   );
 }
 
-function ProductDetail({
-  title,
-  content,
-  learnMore,
-}: {
-  title: string;
-  content: string;
-  learnMore?: string;
-}) {
-  return (
-    <Disclosure key={title} as="div" className="grid w-full gap-2">
-      {({open}) => (
-        <>
-          <Disclosure.Button className="text-left">
-            <div className="flex justify-between">
-              <Text size="lead" as="h4">
-                {title}
-              </Text>
-              <IconClose
-                className={clsx(
-                  'transition-transform transform-gpu duration-200',
-                  !open && 'rotate-[45deg]',
-                )}
-              />
-            </div>
-          </Disclosure.Button>
-
-          <Disclosure.Panel className={'pb-4 pt-2 grid gap-2'}>
-            <div
-              className="prose dark:prose-invert"
-              dangerouslySetInnerHTML={{__html: content}}
-            />
-            {learnMore && (
-              <div className="">
-                <Link
-                  className="pb-px border-b border-primary/30 text-primary/50"
-                  to={learnMore}
-                >
-                  Learn more
-                </Link>
-              </div>
-            )}
-          </Disclosure.Panel>
-        </>
-      )}
-    </Disclosure>
-  );
-}
-
 const PRODUCT_VARIANT_FRAGMENT = `#graphql
   fragment ProductVariantFragment on ProductVariant {
     id
@@ -741,54 +496,6 @@ const PRODUCT_VARIANT_INVENTORY_QUERY = `#graphql
     }
   }
 `;
-
-const RECOMMENDED_PRODUCTS_QUERY = `#graphql
-  ${PRODUCT_CARD_FRAGMENT}
-  query productRecommendations(
-    $productId: ID!
-    $count: Int
-    $country: CountryCode
-    $language: LanguageCode
-  ) @inContext(country: $country, language: $language) {
-    recommended: productRecommendations(productId: $productId) {
-      ...ProductCard
-    }
-    additional: products(first: $count, sortKey: BEST_SELLING) {
-      nodes {
-        ...ProductCard
-      }
-    }
-  }
-`;
-
-async function getRecommendedProducts(
-  storefront: Storefront,
-  productId: string,
-) {
-  const products = await storefront.query<{
-    recommended: ProductType[];
-    additional: ProductConnection;
-  }>(RECOMMENDED_PRODUCTS_QUERY, {
-    variables: {productId, count: 12},
-  });
-
-  invariant(products, 'No data returned from Shopify API');
-
-  const mergedProducts = products.recommended
-    .concat(products.additional.nodes)
-    .filter(
-      (value, index, array) =>
-        array.findIndex((value2) => value2.id === value.id) === index,
-    );
-
-  const originalProduct = mergedProducts
-    .map((item: ProductType) => item.id)
-    .indexOf(productId);
-
-  mergedProducts.splice(originalProduct, 1);
-
-  return mergedProducts;
-}
 
 async function getAllProductVariants(
   storefront: Storefront,
